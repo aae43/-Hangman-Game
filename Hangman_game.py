98fc30f20efa95acc3efcb2a6837db44abaf47d2
Paladin42 <aae42_@hotmail.com>
2022-03-17 23:16:59 -0600

    declaration of the funtion pickrandom and game, and some coments for read words

--- Hangman_game.py
+++ Hangman_game.py
@@ -1,14 +1,26 @@
-def readWords():
-    words = []
+from ast import While
+
+
+def readWords(words):
     with open("./data.txt", "r", encoding= "utf-8")as f:
         for line in f:
-            words.append(str(line))
-        print(words)
+            words.append(line)
+    return words 
+
+def game():
+    pass
     
 
 
 def main():
-    readWords()
+    words = []
+    words = readWords(words)
+    print (words)
+
+    #game_continue = True
+    #while game_continue == True:
+    #    game_continue = game()
+
 
 if __name__ == "__main__":
     main()
